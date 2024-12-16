265e3656dd7b83834117ac72ea661d34bdf377fc
Mr M Chen <145968094+MChenGHS@users.noreply.github.com>
2024-12-16 11:48:58 +0000

    Create T9.py

---
+++ L5/T9.py
@@ -0,0 +1,6 @@
+values = ["Respect", "Excellence", "Friendship"]
+userinput = input("Enter a value of Olympics: ")
+if userinput in values:
+    print("That's correct")
+else:
+    print("Try again")
