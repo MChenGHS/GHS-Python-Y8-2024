5b02133ad61138a72862898da4cbcec6deedd255
Mr M Chen <145968094+MChenGHS@users.noreply.github.com>
2024-12-16 11:44:37 +0000

    Create T7.py

---
+++ L5/T7.py
@@ -0,0 +1,7 @@
+hours = int(input("How long on average do you watch TV every day? "))
+if hours < 2:
+    print("That shouldn't hurt your brain too much")
+elif hours < 4:
+    print("Aren't you getting square eyes?")
+else:
+    print("Fresh air beats channel flicking")
